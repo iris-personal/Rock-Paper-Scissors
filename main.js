d05c11e5a0e6ea99ec3ec72347787141bb7153fb
iris-personal <mary.iris.toland@gmail.com>
2022-10-27 10:58:49 -0500

    Game Logic

--- main.js
+++ main.js
@@ -9,3 +9,27 @@ const choices = ['Lapis', 'Papyrus', 'Scalpellus'];
 
 const randomIndex = Math.floor(Math.random() * choices.length);
 computer.currentChoice = choices[randomIndex];
+player.currentChoice = choices[0]
+
+if (computer.currentChoice === player.currentChoice){
+    console.log("It's a tie!" + computer.currentChoice + player.currentChoice);
+} else if (computer.currentChoice === choices[0]){
+    if (player.currentChoice === choices[1]){
+        console.log("The Player Wins!" + computer.currentChoice + player.currentChoice);
+    } else {
+        console.log("The Computer Wins!" + computer.currentChoice + player.currentChoice);
+    }
+} else if (computer.currentChoice === choices[1]){
+    if (player.currentChoice === choices[0]){
+        console.log("The Computer Wins!" + computer.currentChoice + player.currentChoice);
+    } else {
+        console.log("The Player Wins!" + computer.currentChoice + player.currentChoice);
+    }
+} else {
+    if (player.currentChoice === choices[0]){
+        console.log("The Player Wins!" + computer.currentChoice + player.currentChoice);
+    } else {
+        console.log("The Computer Wins!" + computer.currentChoice + player.currentChoice);
+    }
+}
+
